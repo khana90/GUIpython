ce7fd3972a9831a6965c06d37f9f4ce07cedcae9
A khan <awal.khan@live.co.uk>
2024-09-02 21:56:16 +0000

    updated

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ window.minsize(width=500, height=400)
 
 # Label
 my_labl =Label(text="I am a label", font=("Arial", 24))
-my_labl.pack()
 my_labl["text"] = "New Text"
 my_labl.config(text= "new text")
+# postion text on top column and row
+my_labl.grid(column=0, row=0) 
+# my_labl.pack()
 
 # Button
 def button_clicked():
@@ -33,7 +35,7 @@ spinbox.pack()
 # scale
 def scale_used(value):
   print(value)
-scale=Scale(from_=0, to=100, command=scale_used)
+scale=Scale(from_=0, to=10, command=scale_used)
 scale.pack()
 
 # entries
@@ -52,5 +54,16 @@ text.insert(END, "Example of multi-line text entry")
 # get's current value in textbox at line 1, character
 print(text.get("1.0", END))
 text.pack()
+
+# add a checkbox
+def check_buttonUsed():
+# prints 1 if on, 0 if off
+  print(check_state.get())
+# variable to hold on to checked status
+check_state=IntVar()
+check_button= Checkbutton(text="is on?", variable=check_state, command=check_buttonUsed)
+check_state.get()
+check_button.pack()
+
 
 window.mainloop()
